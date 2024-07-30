911538234d707cd807b8f9ae523af889ffaa0f0c
Bilol <bilol@gmail.com>
2024-07-30 22:08:17 +0500

    end

--- products.js
+++ products.js
@@ -16,3 +16,21 @@ var swiperMain = new Swiper('.mySwiper2', {
         swiper: swiperThumbs,
     },
 });
+
+document.getElementById('model-select').addEventListener('change', function() {
+    const selectedModel = this.value;
+    const modelDetails = document.getElementById('model-details');
+    modelDetails.textContent = `Вы выбрали модель: ${selectedModel}`;
+});
+
+document.getElementById('mount-select').addEventListener('change', function() {
+    const selectedMount = this.value;
+    const mountDetails = document.getElementById('mount-details');
+    mountDetails.textContent = `Вы выбрали вид крепления: ${selectedMount}`;
+});
+
+document.getElementById('kss-select').addEventListener('change', function() {
+    const selectedKss = this.value;
+    const kssDetails = document.getElementById('kss-details');
+    kssDetails.textContent = `Вы выбрали тип КСС: ${selectedKss}`;
+});
