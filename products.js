4dd89849b14406469ae3f2e5622ee4bfc3b0a15f
Bilol <bilol@gmail.com>
2024-07-31 15:54:46 +0500

    add new function

--- products.js
+++ products.js
@@ -34,3 +34,37 @@ document.getElementById('kss-select').addEventListener('change', function() {
     const kssDetails = document.getElementById('kss-details');
     kssDetails.textContent = `Вы выбрали тип КСС: ${selectedKss}`;
 });
+
+const headerLink = document.querySelector(".wrapper__desc-btn")
+
+headerLink.addEventListener("click", function () {
+    document.getElementById("my-modal").classList.add("open")
+})
+
+// Закрыть модальное окно
+document.getElementById("close-my-modal-btn")?.addEventListener("click", function () {
+    document.getElementById("my-modal").classList.remove("open")
+})
+
+// Закрыть модальное окно при нажатии на Esc
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("my-modal").classList.remove("open")
+    }
+});
+
+// Закрыть модальное окно при клике вне его
+document.querySelector("#my-modal .modal__box")?.addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+    
+document.getElementById("my-modal")?.addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
+
+const formBtn = document.querySelector(".form__btn")
+formBtn?.addEventListener("click", function(event){
+    event.preventDefault(); // Предотвращаем отправку формы на сервер
+    console.log("Форма не отправлена");
+});
